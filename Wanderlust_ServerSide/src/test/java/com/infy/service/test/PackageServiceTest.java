a61d4848f1caa38002128c337b8eee63734c346f
siddhi.vasavi <siddhi.vasavi@HYDPCM551631L.ad.infosys.com>
2023-08-17 09:10:45 +0530

    3rd commit

--- Wanderlust_ServerSide/src/test/java/com/infy/service/test/PackageServiceTest.java
+++ Wanderlust_ServerSide/src/test/java/com/infy/service/test/PackageServiceTest.java
@@ -25,4 +25,83 @@ public class PackageServiceTest {
 @Mock
 PackageRepository packageServiceRepository;
 
+@InjectMocks
+PackageServiceImpl p=new PackageServiceImpl();
+@Test
+public void InvalidPackageSearchTest()throws Exception {
+	List<Destination> M=new ArrayList<Destination>();
+	Mockito.when(packageServiceRepository.findByContinent("jaya")).thenReturn(M);
+	Exception e=Assertions.assertThrows(WanderLustException.class,()->p.getPackagesSearch("jaya"));
+	Assertions.assertEquals("PackageService.PACKAGE_UNAVAILABLE",e.getMessage());
+}
+@Test
+public void InvalidPackageDetailSearchTest() throws Exception{
+	Destination D=new Destination();
+	D.setDestinationId("D1001");
+	D.setContinent("Asia");
+	Itinerary i=new Itinerary();
+	i.setItineraryId("I1001");
+	Details d=new Details();
+			d.setDetailsId("DL101");
+			d.setItinerary(i);
+			D.setDetails(d);
+			List<Destination> dest1=new ArrayList<>();
+			dest1.add(D);
+			Mockito.when(packageServiceRepository.findByContinent("Asia")).thenReturn(dest1);
+		
+			List<DestinationDTO> list=p.getPackagesSearch("Asia");
+			Assertions.assertNotNull(list);
+			
+}
+@Test
+void InvalidGetItineraryTest() throws Exception {
+	Destination D=new Destination();
+	Mockito.when(packageServiceRepository.findById(D.getDestinationId())).thenReturn(null);
+	WanderLustException exception=Assertions.assertThrows(WanderLustException.class,()->p.getItinerary("5123"));
+	Assertions.assertEquals("PackageService.ITINERARY_UNAVAILABLE", exception.getMessage());
+}
+void validGetItineraryTest() throws Exception {
+	Destination D=new Destination();
+	D.setDestinationId("D1001");
+	D.setContinent("Asia");
+	Itinerary i=new Itinerary();
+	i.setItineraryId("I1001");
+	Details d=new Details();
+			d.setDetailsId("DL101");
+			d.setItinerary(i);
+			D.setDetails(d);
+			Mockito.<Optional<Destination>>when(packageServiceRepository.findById("D1001")).thenReturn(Optional.of(D));
+			DestinationDTO dto=p.getItinerary("D1001");
+			Assertions.assertNotNull(dto);
+			
+}
+@Test
+void validGetPackages() throws Exception{
+	Destination D=new Destination();
+	D.setDestinationId("D1001");
+	D.setContinent("Asia");
+	Itinerary i=new Itinerary();
+	i.setItineraryId("I1001");
+	Details d=new Details();
+			d.setDetailsId("DL101");
+			d.setItinerary(i);
+			D.setDetails(d);
+			List<Destination> destlist=new ArrayList<Destination>();
+			destlist.add(D);
+			Mockito.<Iterable<Destination>>when(packageServiceRepository.findAll()).thenReturn(destlist);
+			List<DestinationDTO> list=p.getPackages();
+			Assertions.assertNotNull(list);
+}
+@Test
+void InvalidGetPackages() throws Exception{
+	DestinationDTO D=new DestinationDTO();
+	D.setDestinationId("666666");
+	Mockito.when(packageServiceRepository.findAll()).thenReturn(List.of());
+	WanderLustException exception=Assertions.assertThrows(WanderLustException.class,()->p.getPackages());
+	Assertions.assertEquals("PackageService.PACKAGE_UNAVAILABLE", exception.getMessage());
+}
+
+}
+
+
 }
--- Wanderlust_ServerSide/src/test/java/com/infy/service/test/UserServiceTest.java
+++ Wanderlust_ServerSide/src/test/java/com/infy/service/test/UserServiceTest.java
@@ -34,4 +34,105 @@ public class UserServiceTest {
 		user.setPassword("xyz");
 
 
-	}}
+		Mockito.when(userRepository.findByContactNumber(contactNo)).thenReturn(user);
+		WanderLustException exception = Assertions.assertThrows(WanderLustException.class,
+				() -> userServiceImpl.authenticateUser(contactNo, password));
+		Assertions.assertEquals("UserService.INVALID_CREDENTIALS", exception.getMessage());
+
+	}
+	
+	@Test
+	public void validAuthenticateUser() throws Exception{
+		
+		UserDTO userdto= new UserDTO();
+		userdto.setContactNumber("9899757623");
+		userdto.setEmailId("Martin@gmail.com");
+		userdto.setPassword("Martin@123");
+		userdto.setUserName("Martin");
+		userdto.setUserId(301);
+		
+		
+		User user= new User();
+	    user.setContactNumber(userdto.getContactNumber());
+	    user.setEmailId(userdto.getEmailId());
+	    String hashcode = HashingUtility.getHashValue("Martin@123");
+	    user.setPassword(hashcode);
+	    user.setUserName(userdto.getUserName());
+	    user.setUserId(userdto.getUserId());
+	   
+	    
+	    Mockito.when(userRepository.findByContactNumber(userdto.getContactNumber())).thenReturn(user);
+	    UserDTO userDTO= userServiceImpl.authenticateUser(userdto.getContactNumber(),userdto.getPassword());
+	    Assertions.assertEquals(userdto.getUserId(),userDTO.getUserId());
+	}
+	@Test
+	public void noUserFoundAuthenticateUserTest() throws Exception{
+		Mockito.when(userRepository.findByContactNumber("8823458890")).thenReturn(null);
+	    WanderLustException e= Assertions.assertThrows(WanderLustException.class,()->userServiceImpl.authenticateUser("8823458890","Amazon@1234"));
+	    Assertions.assertEquals("UserService.INVALID_CREDENTIALS",e.getMessage());
+		
+	}
+	@Test
+	public void nullUserPassword()throws Exception{
+		UserDTO userdto= new UserDTO();
+		userdto.setContactNumber("9899757623");
+		userdto.setEmailId("Martin@gmail.com");
+		userdto.setPassword("Martin@123");
+		userdto.setUserId(100);
+		userdto.setUserName("Martin");
+		
+		User user= new User();
+	    user.setContactNumber(userdto.getContactNumber());
+	    user.setEmailId(userdto.getEmailId());
+	    user.setPassword(null);
+	    user.setUserId(userdto.getUserId());
+	    user.setUserName(userdto.getUserName());
+	    
+	    Mockito.when(userRepository.findByContactNumber(userdto.getContactNumber())).thenReturn(user);
+	    Exception exp= Assertions.assertThrows(WanderLustException.class,()->userServiceImpl.authenticateUser(userdto.getContactNumber(),userdto.getPassword()));
+	    Assertions.assertEquals("UserService.INVALID_CREDENTIALS",exp.getMessage());
+		
+	}
+	@Test
+	public void validRegisterUser() throws Exception{
+		UserDTO userdto= new UserDTO();
+		userdto.setContactNumber("8875632143");
+		userdto.setEmailId("jimmy@stark.com");
+		userdto.setPassword("Jimmy@123");
+		userdto.setUserName("JIMMY");
+		
+		User user= new User();
+	    user.setContactNumber(userdto.getContactNumber());
+	    user.setEmailId(userdto.getEmailId());
+	    user.setPassword(HashingUtility.getHashValue(userdto.getPassword()));
+	    user.setUserName("JIMMY");
+	    
+	    
+	    Mockito.when(userRepository.findByContactNumber(userdto.getContactNumber())).thenReturn(null);
+	    Mockito.when(userRepository.save(user)).thenReturn(user);
+	    Integer id= userServiceImpl.registerUser(userdto);
+	    Assertions.assertNull(id);
+	}
+	@Test
+	public void userAlreadyExistTest() throws Exception{
+		UserDTO userdto= new UserDTO();
+		userdto.setContactNumber("8765667898");
+		userdto.setEmailId("jerry23@gmail.com");
+		userdto.setPassword("Jerry@123");
+		userdto.setUserName("Jerry");
+		
+		User user= new User();
+	    user.setContactNumber(userdto.getContactNumber());
+	    user.setEmailId(userdto.getEmailId());
+	    user.setPassword(HashingUtility.getHashValue(userdto.getPassword()));
+	    user.setUserName(userdto.getUserName());
+	    
+	    Mockito.when(userRepository.findByContactNumber(userdto.getContactNumber())).thenReturn(user);
+	    WanderLustException e= Assertions.assertThrows(WanderLustException.class,()->userServiceImpl.registerUser(userdto));
+	    Assertions.assertEquals("UserService.CONTACT_NUMBER_ALREADY_EXISTS",e.getMessage());	
+	}
+	}
+
+		
+		
+	
